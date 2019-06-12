63b405d6c0affeca789c25bf9c1723218c7105dd
Bnp1997 <bnp1997@gmail.com>
2019-06-12 21:02:03 +0200

    now game i runnable but still has errors

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+import processing.core.PApplet;
 
+public class Main extends PApplet {
+
+    Program program;
+    Counters counters;
+    int speed = 1;
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+
+    public void settingScreenSize() {
+        size(1920, 1080);
+    }
+
+    public void programSetup() {
+        counters = new Counters(this);
+        program = new Program(this, counters);
+        program.setup();
+        frameRate(999);
+    }
+
+    public void draw() {
+        background(0);
+        program.display();
     }
 }
